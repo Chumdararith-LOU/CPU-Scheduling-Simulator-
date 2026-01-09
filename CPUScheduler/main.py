a558f133fbc9e107f108fa42a7993e50b1187969
Dararith <louchumdararith02@gmail.com>
2026-01-09 22:43:31 +0700

    Feat: Implement FCFS algorithm and results display table

--- CPUScheduler/main.py
+++ CPUScheduler/main.py
@@ -1,5 +1,6 @@
 import csv
 from process import Process
+from scheduler import solve_fcfs
 
 def load_processes(filename):
     processes = []
@@ -22,10 +23,31 @@ def load_processes(filename):
     except ValueError:
         print("Error: Invalid data format in CSV. Ensure numbers are integers.")
         return []
+    
+def print_results(processes, gantt):
+    print("\nPID\tArrival\tBurst\tFinish\tWait\tTurnaround\tResponse")
+    print("-" * 65)
+    
+    total_wait = 0
+    total_turnaround = 0
+    total_response = 0
+    
+    for p in processes:
+        print(f"{p.pid}\t{p.arrival_time}\t{p.burst_time}\t{p.completion_time}\t"
+              f"{p.waiting_time}\t{p.turnaround_time}\t\t{p.response_time}")
+        
+        total_wait += p.waiting_time
+        total_turnaround += p.turnaround_time
+        total_response += p.response_time
+        
+    n = len(processes)
+    print("-" * 65)
+    print(f"Averages:\t\t\t{total_wait/n:.2f}\t{total_turnaround/n:.2f}\t\t{total_response/n:.2f}")
 
 if __name__ == "__main__":
     process_list = load_processes("input.csv")
     
-    print("\n--- Current Process List ---")
-    for p in process_list:
-        print(p)
+    if process_list:
+        fcfs_result, fcfs_gantt = solve_fcfs(process_list[:])
+        
+        print_results(fcfs_result, fcfs_gantt)
---
+++ CPUScheduler/scheduler.py
@@ -0,0 +1,24 @@
+def solve_fcfs(processes):
+    print("--- Running FCFS Algorithm ---")
+    
+    processes.sort(key=lambda p: p.arrival_time)
+    
+    current_time = 0
+    gantt_data = [] 
+    
+    for p in processes:
+        if current_time < p.arrival_time:
+            current_time = p.arrival_time
+            
+        p.start_time = current_time
+        p.completion_time = current_time + p.burst_time
+        
+        p.turnaround_time = p.completion_time - p.arrival_time
+        p.waiting_time = p.turnaround_time - p.burst_time
+        p.response_time = p.start_time - p.arrival_time
+        
+        gantt_data.append((p.pid, p.start_time, p.completion_time))
+        
+        current_time = p.completion_time
+        
+    return processes, gantt_data
